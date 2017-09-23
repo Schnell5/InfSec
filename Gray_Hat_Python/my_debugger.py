b2f22f506e54aa6bd049f290bc5332f7049e73d2
Schnell5 <32035281+Schnell5@users.noreply.github.com>
2017-09-24 00:31:27 +0300

    Added changes

--- Gray_Hat_Python/my_debugger.py
+++ Gray_Hat_Python/my_debugger.py
@@ -3,6 +3,7 @@ from ctypes import windll
 from _ctypes import sizeof, byref
 from _winapi import PROCESS_ALL_ACCESS
 from builtins import int
+from distutils.log import debug
 
 kernel32 = windll.kernel32
 
@@ -17,8 +18,8 @@ class debugger():
         # dwCreation flag determines how to create the process
         # set creation_flags = CREATE_NEW_CONSOLE if you want
         # to see the calculator GUI
-        #creation_flags = DEBUG_PROCESS
-        creation_flags = CREATE_NEW_CONSOLE
+        creation_flags = DEBUG_PROCESS
+        #creation_flags = CREATE_NEW_CONSOLE
         
         # instantiate the structs
         startupinfo = STARTUPINFO()
@@ -51,6 +52,7 @@ class debugger():
             
             # Obtain a valid handle to the newly created process and store it for future access
             self.h_process = self.open_process(process_information.dwProcessId)
+            #self.attach(process_information.dwProcessId)
             
         else:
             print("[*] Error 0x{0:08x}".format(kernel32.GetLastError()))
@@ -60,6 +62,59 @@ class debugger():
         h_process = kernel32.OpenProcess(PROCESS_ALL_ACCESS, False, pid)
         return h_process
     
+    def open_thread(self, thread_id):
+        
+        h_thread = kernel32.OpenThread(THREAD_ALL_ACCESS, None, thread_id)
+        
+        if h_thread is not None:
+            return h_thread
+        
+        else:
+            print("[!] Could not obtain a valid thread handler.")
+            return False
+        
+    def enumerate_threads(self):
+        
+        thread_entry = THREADENTRY32()
+        thread_list = []
+        snapshot = kernel32.CreateToolhelp32Snapshot(TH32CS_SNAPTHREAD, self.pid)
+        
+        if snapshot is not None:
+            # You have to set the size of the struct or the call will fail
+            thread_entry.dwSize = sizeof(thread_entry)
+            success = kernel32.Thread32First(snapshot, byref(thread_entry))
+            
+            while success:
+                #print("self.pid = ", self.pid)
+                #print("thread_entry.th32OwnerProcessID = ", thread_entry.th32OwnerProcessID)
+                if thread_entry.th32OwnerProcessID == self.pid:
+                    thread_list.append(thread_entry.th32ThreadID)
+                    print("thread_lits_new", thread_list)
+                    success = kernel32.Thread32Next(snapshot, byref(thread_entry))
+            
+            kernel32.CloseHandle(snapshot)
+            print(thread_list)
+            return thread_list
+        else:
+            print("[!] Could not get the snapshot.")
+            return False
+        
+    def get_thread_context(self, thread_id = None, h_thread = None):
+        
+        context = CONTEXT()
+        context.ContextFlags = CONTEXT_FULL | CONTEXT_DEBUG_REGISTERS
+        
+        # Obtain a handle to the thread
+        if not h_thread:
+            self.open_thread(thread_id)
+            
+        if kernel32.GetThreadContext(h_thread, byref(context)):
+            kernel32.CloseHandle(h_thread)
+            return context
+        else:
+            print("[!] Could not get the context of the thread.")
+            return False
+        
     def attach(self, pid):
         
         self.h_process = self.open_process(pid)
@@ -75,8 +130,8 @@ class debugger():
             print("[*] Unable to attach to the process.")
             
         
-        while self.debugger_active == True:
-            self.get_debug_event()
+        #while self.debugger_active == True:
+        self.get_debug_event()
     
     def get_debug_event(self):
         
@@ -85,9 +140,10 @@ class debugger():
         
         if kernel32.WaitForDebugEvent(byref(debug_event), INFINITE):
             
-            input("Press a key to continue...")
-            self.debugger_active = False
+            #input("Press a key to continue...")
+            #self.debugger_active = False
             kernel32.ContinueDebugEvent(debug_event.dwProcessId, debug_event.dwThreadId, continue_status)
+            print(debug_event.dwProcessId, debug_event.dwThreadId, continue_status)
             
     def detach(self):
         
--- Gray_Hat_Python/my_test.py
+++ Gray_Hat_Python/my_test.py
@@ -9,6 +9,26 @@ pid = int(input("Enter the PID of the process to attach to: "))
 
 debugger.attach(pid)
 
+list_threads = debugger.enumerate_threads()
+
+# For each thread in the list we want to grab the
+# value of each of the registers
+
+for thread in list_threads:
+    
+    thread_context = debugger.get_thread_context(thread)
+    
+    # Now let's output the contents of some of the registers    
+    print("[*] Dumping registers for thread ID 0x{0:08x}".format(thread))
+    print("[**] EIP: 0x{0:08x}".format(thread_context.Eip))
+    print("[**] ESP: 0x{0:08x}".format(thread_context.Esp))
+    print("[**] EBP: 0x{0:08x}".format(thread_context.Ebp))
+    print("[**] EAX: 0x{0:08x}".format(thread_context.Eax))
+    print("[**] EBX: 0x{0:08x}".format(thread_context.Ebx))
+    print("[**] ECX: 0x{0:08x}".format(thread_context.Ecx))
+    print("[**] EDX: 0x{0:08x}".format(thread_context.Edx))
+    print("[*] END DUMP")
+    
 debugger.detach()
 
 
