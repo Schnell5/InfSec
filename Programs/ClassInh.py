c0b29b16ac251fd6a863d042c1b6aae183000e46
Schnell5 <32035281+Schnell5@users.noreply.github.com>
2018-01-04 01:18:07 +0300

    little changes

--- Programs/ClassInh.py
+++ Programs/ClassInh.py
@@ -2,12 +2,14 @@ class Person:
     def __init__(self, weight, hight):
         self.weight = weight
         self.hight = hight
+
     def say(self, string):
         print ("Hello, my name is {}".format(string))
 
 class Student(Person):
     def __init__(self, weight, height):
         super().__init__(weight, height)
+
     def course(self, course):
         self.course = course
 
