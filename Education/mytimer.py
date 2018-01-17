959c75039de6282e6b761eeeed4149c8f070994a
schnell_mac <schnell5@mail.ru>
2018-01-17 22:07:06 +0300

    New file added

---
+++ Education/mytimer.py
@@ -0,0 +1,20 @@
+"""
+Counting the time on the function
+"""
+
+import time
+
+reps = 1000
+replist = range(reps)
+
+
+def timer(func, *args, **kwargs):
+    start = time.clock()
+    for _ in replist:
+        ret = func(*args, *kwargs)
+    elapsed = time.clock() - start
+
+    return elapsed, ret
+
+
+
