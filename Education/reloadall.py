00b29286e8f985a1b47a5304fa359206202f0d44
schnell_mac <schnell5@mail.ru>
2018-01-21 00:13:19 +0300

    New file

---
+++ Education/reloadall.py
@@ -0,0 +1,33 @@
+'''
+Reload all modules recursively
+'''
+
+import types
+from importlib import reload
+
+
+def status(module):
+    print('reloading ' + module.__name__)
+
+
+def transitive_reload(module, visited):
+    if module not in visited:
+        status(module)
+        reload(module)
+        visited[module] = None
+        for attrobj in module.__dict__.values():
+            if isinstance(attrobj, types.ModuleType):
+                transitive_reload(attrobj, visited)
+
+
+def reload_all(*args):
+    visited = {}
+    for arg in args:
+        if isinstance(arg, types.ModuleType):
+            transitive_reload(arg, visited)
+
+
+if __name__ == '__main__':
+    import reloadall
+    reload_all(reloadall)
+
