faebaa03b40bd84ee88d4eb9b094d7b391ba9e2b
Schnell5 <32035281+Schnell5@users.noreply.github.com>
2018-03-18 13:25:07 +0300

    photo viewer

---
+++ Practice/GUI/viewer_thumbs.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import math
+from tkinter import *
+from PIL import Image
+from PIL.ImageTk import PhotoImage
+
+
+def makeThumbs(imgdir, size=(100, 100), subdir='thumbs'):
+    thumbdir = os.path.join(imgdir, subdir)
+    if not os.path.exists(thumbdir):
+        os.mkdir(thumbdir)
+
+    thumbs = []
+    for imgfile in os.listdir(imgdir):
+        thumbpath = os.path.join(thumbdir, imgfile)
+        if os.path.exists(thumbpath):
+            thumbobj = Image.open(thumbpath)
+            thumbs.append((imgfile, thumbobj))
+        else:
+            print('making', thumbpath)
+            imgpath = os.path.join(imgdir, imgfile)
+            try:
+                imgobj = Image.open(imgpath)
+                imgobj.thumbnail(size, Image.ANTIALIAS)
+                imgobj.save(thumbpath)
+                thumbs.append((imgfile, imgobj))
+            except Exception:
+                print('Skipping', imgpath)
+    return thumbs
+
+
+class ViewOne(Toplevel):
+    def __init__(self, imgdir, imgfile):
+        Toplevel.__init__(self)
+        self.title(imgfile)
+        imgpath = os.path.join(imgdir, imgfile)
+        imgobj = PhotoImage(file=imgpath)
+        Label(self, image=imgobj).pack()
+        print(imgpath, imgobj.width(), imgobj.height())
+        self.savephoto = imgobj
+
+
+def viewer(imgdir, kind=Toplevel, cols=None):
+    win = kind()
+    win.title('Viewer: ' + imgdir)
+    quitbtn = Button(win, text='Quit', command=win.quit, bg='beige')
+    quitbtn.pack(fill=X, side=BOTTOM)
+    thumbs = makeThumbs(imgdir)
+    if not cols:
+        cols = int(math.ceil(math.sqrt(len(thumbs))))
+
+    savephotos = []
+    while thumbs:
+        thumbsrow, thumbs = thumbs[:cols], thumbs[cols:]
+        row = Frame(win)
+        row.pack(fill=BOTH)
+        for (imgfile, imgobj) in thumbsrow:
+            photo = PhotoImage(imgobj)
+            link = Button(row, image=photo)
+            handler = lambda savefile=imgfile: ViewOne(imgdir, savefile)
+            link.config(command=handler)
+            link.pack(side=LEFT, expand=YES)
+            savephotos.append(photo)
+    return win, savephotos
+
+
+if __name__ == '__main__':
+    defdir = r'C:\Users\1\Google Диск\PYTHON\PP4E-Examples-1.4\Examples\PP4E\Gui\PIL\images'
+    imgdir = (len(sys.argv) > 1 and sys.argv[1]) or defdir
+    main, save = viewer(imgdir, kind=Tk)
+    main.mainloop()
