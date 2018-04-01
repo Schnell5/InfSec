569891f95fe28d9c93323a5d4b12f28895bfffa2
Schnell5 <32035281+Schnell5@users.noreply.github.com>
2018-04-01 19:08:20 +0300

    Dynamic updates

---
+++ Practice/GUI/rad.py
@@ -0,0 +1,28 @@
+from tkinter import *
+import radactions
+from importlib import reload
+
+
+class Hello(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.pack()
+        self.makeWidgets()
+
+    def makeWidgets(self):
+        Button(self, text='message1', command=self.message1).pack(side=LEFT)
+        Button(self, text='message2', command=self.message2).pack(side=RIGHT)
+
+    def message1(self):
+        reload(radactions)
+        radactions.message1()
+
+    def message2(self):
+        reload(radactions)
+        radactions.message2(self)
+
+    def method1(self):
+        print('exposed method...')
+
+
+Hello().mainloop()
---
+++ Practice/GUI/radactions.py
@@ -0,0 +1,7 @@
+def message1():
+    print('Hello world!!!')
+
+
+def message2(self):
+    print('!Spam!')
+    self.method1()
