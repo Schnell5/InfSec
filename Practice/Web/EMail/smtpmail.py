db2010ed3138b0ded5353e6fa00fd00ada6b0951
schnell_mac <schnell5@mail.ru>
2018-05-02 18:15:20 +0300

    SMTP - send email to server

---
+++ Practice/Web/EMail/smtpmail.py
@@ -0,0 +1,34 @@
+import smtplib
+import email.utils
+import sys
+import mailconfig
+import getpass
+
+
+mailserver = mailconfig.smtpservername
+
+From = input('From: ').strip()
+To = input('To: ').strip()
+Tos = To.split(';')
+Subj = input('Subject: ').strip()
+Date = email.utils.formatdate()
+
+text = 'From: {0}\nTo: {1}\nDate: {2}\nSubject: {3}\n\n'.format(From, To, Date, Subj)
+print('Type message text, end with line=[Ctrl+d (Unix), Ctrl+z (Windows)]')
+while True:
+    line = sys.stdin.readline()
+    if not line:
+        break       # Exit (Ctrl+d/z)
+    text += line
+
+print('Connecting...')
+server = smtplib.SMTP(mailserver)
+#server.login(mailconfig.smtpusername, getpass.getpass('Password:'))    # If authentication required
+failed = server.sendmail(From, Tos, text)
+server.quit()
+
+if failed:
+    print('Failed recipients:', failed)
+else:
+    print('No errors')
+print('Bye.')
