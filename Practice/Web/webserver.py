ff2836d268c43ddba1689421c46944ddb0303b83
schnell_mac <schnell5@mail.ru>
2018-06-28 23:25:51 +0300

    Webserver

--- Practice/Web/webserver.py
+++ Practice/Web/webserver.py
@@ -4,6 +4,12 @@ from http.server import HTTPServer, CGIHTTPRequestHandler
 
 webdir = '.'
 port = 80
+
+if len(sys.argv) > 1:
+    webdir = sys.argv[1]
+if len(sys.argv) > 2:
+    port = sys.argv[2]
+
 os.chdir(webdir)
 srvaddr = ("", port)
 srvobj = HTTPServer(srvaddr, CGIHTTPRequestHandler)
